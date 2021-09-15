d07c0279d53063efc9ba3ce0c5d3567c4e7fd3eb
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2021-09-15 14:05:01 +0000

    Update fibrose_model.py

--- Projet_mboup/fibrose_model.py
+++ Projet_mboup/fibrose_model.py
@@ -14,7 +14,7 @@ from PIL import Image, ImageDraw
 import numpy as np
 import os
 class LitModel(pl.LightningModule):
-    def __init__(self, input_shape, num_classes, learning_rate=1e-4):
+    def __init__(self, input_shape, num_classes, learning_rate=3e-4):
         super().__init__()
         
         # log hyperparameters
