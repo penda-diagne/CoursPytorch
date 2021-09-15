02edb3edc8d36280c8b18d9b2c94fd3db86f82ee
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2021-09-15 14:17:19 +0000

    Update fibrose_model.py

--- Projet_mboup/fibrose_model.py
+++ Projet_mboup/fibrose_model.py
@@ -98,5 +98,5 @@ class LitModel(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
