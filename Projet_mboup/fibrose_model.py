dde8ee364d5c6a1f4e2f2fdc637dda91e190cef2
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2021-10-03 22:39:45 +0000

    Update fibrose_model.py

--- Projet_mboup/fibrose_model.py
+++ Projet_mboup/fibrose_model.py
@@ -14,7 +14,7 @@ from PIL import Image, ImageDraw
 import numpy as np
 import os
 class LitModel(pl.LightningModule):
-    def __init__(self, input_shape, num_classes, learning_rate=1e-4):
+    def __init__(self, input_shape, num_classes, learning_rate=3e-4):
         super().__init__()
         
         # log hyperparameters
@@ -27,7 +27,7 @@ class LitModel(pl.LightningModule):
         self.feature_extractor.eval()
         layer4 = self.feature_extractor.layer4
         self.feature_extractor.layer4 = nn.Sequential(
-                                    nn.Dropout(0.6),
+                                    nn.Dropout(0.3),
                                     layer4
                                     )
         for param in self.feature_extractor.parameters():
@@ -98,5 +98,5 @@ class LitModel(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
