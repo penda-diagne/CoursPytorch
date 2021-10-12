c97a12d0b2d1890dc7a81366b6f678508f4b4459
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2021-10-12 22:57:38 +0000

    Update fibrose_data_module.py

--- Projet_mboup/fibrose_data_module.py
+++ Projet_mboup/fibrose_data_module.py
@@ -43,7 +43,7 @@ class Caltech101DataModule(pl.LightningDataModule):
         self.train.dataset.transform = self.augmentation
         self.val.dataset.transform = self.transform
         self.test.dataset.transform = self.transform
-        return self.train.dataset.transform, self.val.dataset.transform, self.test.dataset.transform = self.transform
+        return self.train.dataset.transform, self.val.dataset.transform, self.test.dataset.transform
         
     def train_dataloader(self):
         return DataLoader(self.train, batch_size=self.batch_size, shuffle=True)
