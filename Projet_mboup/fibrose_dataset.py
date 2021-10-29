037a552b9d1fe89bd847c4c1c527d9583f527bda
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2021-10-29 18:35:04 +0000

    Update fibrose_dataset.py

--- Projet_mboup/fibrose_dataset.py
+++ Projet_mboup/fibrose_dataset.py
@@ -31,9 +31,10 @@ class mboupDataset(Dataset):
     Ouvre  l'image numero i, applique le transform et retourne avec le label
     '''
     img = Image.open(data[i])
-    img_r = img.resize((329,375))
-    img_cr = img_r.crop((0, 120, 329, 375))
-    img_f = img_cr.filter(ImageFilter.SMOOTH)
-    img_t = self.transform(img_f)
-    return img_t,self.labels[i]
+    img = img.resize((329,375))
+    img = img.crop((0, 100, 329, 375))
+    img = img.resize((329,375))
+    img = img.filter(ImageFilter.SMOOTH)
+    img = self.transform(img)
+    return img,self.labels[i]
     
