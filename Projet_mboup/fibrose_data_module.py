4f8de411d385f807f08f9905ce4b500413f92431
penda-diagne <59060758+penda-diagne@users.noreply.github.com>
2021-10-29 00:23:56 +0000

    Update fibrose_data_module.py

--- Projet_mboup/fibrose_data_module.py
+++ Projet_mboup/fibrose_data_module.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import pytorch_lightning as pl
 import torchvision.transforms as transforms
-import torchmetrics.functionnal.accuracy as accuracy
+import torchmetrics.functional.accuracy as accuracy
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 import torchvision.models as models
